18e38d14bf9cf3d522cba4fe2c4d7e3e89221b56
P R <gokul.p-r@capgemini.com>
2024-10-18 13:23:20 +0530

    added eureka server and fixed minor issues

--- finance-management-service/src/main/java/com/main/repository/TransactionRepository.java
+++ finance-management-service/src/main/java/com/main/repository/TransactionRepository.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.main.dto.User;
 import com.main.entity.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer>{
-	List<Transaction> findByUser(User user);
+	List<Transaction> findByUserId(int userId);
 }

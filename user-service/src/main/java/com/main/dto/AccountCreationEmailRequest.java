e2378e0efe2cece270d36a98e1e54d110429cdce
S <krishnan.c.s@capgemini.com>
2024-10-19 13:04:06 +0530

    added logger in user service

--- user-service/src/main/java/com/main/dto/AccountCreationEmailRequest.java
+++ user-service/src/main/java/com/main/dto/AccountCreationEmailRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmailRequest {
+public class AccountCreationEmailRequest {
 
 	private String toEmail;
 	private String subject;
--- user-service/src/main/java/com/main/service/UserServiceImpl.java
+++ user-service/src/main/java/com/main/service/UserServiceImpl.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.main.client.NotificationClient;
-import com.main.dto.EmailRequest;
+import com.main.dto.AccountCreationEmailRequest;
 import com.main.dto.UserProfile;
 import com.main.dto.UserProfileUpdateRequest;
 import com.main.dto.UserRegistrationRequest;
@@ -20,6 +22,10 @@ import com.main.repository.UserRepository;
 @Service
 public class UserServiceImpl implements UserService{
 	
+private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+	
+	private static final String USER_NOT_FOUND_MSG = "User profile not found with id: ";
+	
 	@Autowired
 	private UserRepository userRepo;
 	
@@ -29,6 +35,8 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public UserProfile addNewUsers(UserRegistrationRequest addUsers) 
 	{
+		logger.info("Adding a new user with username: {}", addUsers.getUserName());
+		
 		User user = new User();
 		user.setUserName(addUsers.getUserName());
 		user.setPassword(addUsers.getPassword());
@@ -37,9 +45,13 @@ public class UserServiceImpl implements UserService{
         
         User savedUser = userRepo.save(user);
         
-        EmailRequest emailrequest = new EmailRequest(savedUser.getEmail(), "Account Created", "Account created, thank you for choosing us.");
+        logger.debug("{} saved successfully with id: {}", savedUser.getRole(),savedUser.getUserId());
+        
+        AccountCreationEmailRequest emailrequest = new AccountCreationEmailRequest(savedUser.getEmail(), "Account Created", "Account created, thank you for choosing us.");
         
         notificationClient.sendAccountCreationEmail(emailrequest);
+        
+        logger.info("Account Creation email sent to {}", savedUser.getEmail());
         
         return new UserProfile(savedUser.getUserId(), savedUser.getUserName(), savedUser.getEmail(), savedUser.getRole(), savedUser.getWallet());
 	}
@@ -47,11 +59,14 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public List<User> getAllUsers() 
 	{	
+		logger.info("Fetching all users from the database.");
 		return userRepo.findAll();
 	}
 
 	@Override
 	public UserProfile updateUserProfile(int userId, UserProfileUpdateRequest u) {
+		
+		logger.info("Updating profile for user with id: {}", userId);
 		
 		// first checking is user present
 		Optional<User> userOpt = userRepo.findById(userId);
@@ -66,36 +81,54 @@ public class UserServiceImpl implements UserService{
 			existingUser.setUpdatedAt(LocalDate.now()); // updating the date
 			// save the updates and return it
 			User updatedUser = userRepo.save(existingUser);
+			logger.info("User profile updated successfully for userId: {}", userId);
+			
 			return new UserProfile(updatedUser.getUserId(), updatedUser.getUserName(), updatedUser.getEmail(), updatedUser.getRole(), updatedUser.getWallet());
 		}
-		throw new UserNotFoundException("User not found with id: " + userId);
+		else
+		{
+		logger.error("User with id {} not found for profile update", userId);
+		throw new UserNotFoundException(USER_NOT_FOUND_MSG + userId);
+		}
 	}
 
 	@Override
 	public UserProfile getUserById(int userId) {
 		
+		logger.info("Fetching Profile by id: {}", userId);
+		
 		Optional<User> user = userRepo.findById(userId);
 		if(user.isPresent())
 		{
 			User userEntity = user.get();
-			System.out.println(userEntity.getWallet());
+			
+			logger.debug("Found user with Id: {} and wallet balance: {}", userEntity.getUserId(), userEntity.getWallet());
+			
 			return new UserProfile(userEntity.getUserId(), userEntity.getUserName(), userEntity.getEmail(), userEntity.getRole(), userEntity.getWallet());
 		}
-		throw new UserNotFoundException("User not found with id: " + userId);
+		else
+		{
+			logger.error("User with id {} not found", userId);
+			throw new UserNotFoundException(USER_NOT_FOUND_MSG + userId);
+		}
 	}
 
 	@Override
 	public boolean deleteUser(int userId) {
+		
+		logger.info("Deleting user with id: {}", userId);
 		 
 		Optional<User> user = userRepo.findById(userId);
 		
 		if (user.isPresent()) {
             userRepo.delete(user.get());
+            logger.info("User profile with Id: {} deleted successfully", userId);
             return true; // Deleted the user successfully and return true
         } 
 		else 
 		{
-            throw new UserNotFoundException("User not found with id: " + userId);
+			logger.error("User profile with Id: {} not found for deletion", userId);
+            throw new UserNotFoundException(USER_NOT_FOUND_MSG + userId);
 		}
 		
 	}

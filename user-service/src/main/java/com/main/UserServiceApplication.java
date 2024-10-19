9d109d1331c67372dd96b03ac04081ed2ae5ee51
S <krishnan.c.s@capgemini.com>
2024-10-19 15:25:23 +0530

    password protected

--- user-service/src/main/java/com/main/UserServiceApplication.java
+++ user-service/src/main/java/com/main/UserServiceApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 }

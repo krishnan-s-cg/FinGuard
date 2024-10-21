e16595d330b991ecc2516a872a3a31be77c00d97
S <krishnan.c.s@capgemini.com>
2024-10-21 12:17:15 +0530

    fix: gateway issue sorted

--- gate-way-secure-role-based/src/main/java/com/cg/config/SecurityConfig.java
+++ gate-way-secure-role-based/src/main/java/com/cg/config/SecurityConfig.java
@@ -23,13 +23,12 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
             .authorizeExchange(exchanges -> exchanges
-           //     .pathMatchers("/public/**").permitAll()   // Public routes, no auth required
-              //  .pathMatchers("/user-service/finguard/user/login", "/user-service/finguard/user/register").permitAll() // Allow access without authentication
-               // .pathMatchers(HttpMethod.PUT,"/user-service/finguard/user/wallet/**").permitAll()
-              //  .pathMatchers(HttpMethod.POST,"/user-service/finguard/user").hasRole("ROLE_user")
+                .pathMatchers("/public/**").permitAll()   // Public routes, no auth required
+                .pathMatchers("/user-service/finguard/user/login", "/user-service/finguard/user/register").permitAll() // Allow access without authentication
+                .pathMatchers(HttpMethod.POST,"/user-service/finguard/user").hasRole("ROLE_user")
                 .pathMatchers(HttpMethod.PUT,"/user-service/finguard/user/wallet/**").hasRole("ROLE_user")
-                //.pathMatchers(HttpMethod.GET,"/user-service/finguard/users").hasRole("ROLE_admin")	
-           //     .pathMatchers(HttpMethod.GET,"/user-service/finguard/user/**").permitAll()
+                .pathMatchers(HttpMethod.GET,"/user-service/finguard/users").hasRole("ROLE_admin")	
+                .pathMatchers(HttpMethod.GET,"/user-service/finguard/user/**").hasRole("ROLE_user")
 //                .pathMatchers("/order-service/loadbalance/**").hasRole("ROLE_admin") // ADMIN role only for /admin/**
 //                .pathMatchers("/order-service/feign/**").hasRole("ROLE_user")   // USER role only for /user/**
                 .anyExchange().authenticated()              // All other routes require authentication
